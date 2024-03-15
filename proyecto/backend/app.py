891f23760f55c1147bd22da5ef2edcd857f96048
PochaOK <sebastianfernandez2812@gmail.com>
2024-03-15 11:21:52 -0300

    some changes

--- proyecto/backend/app.py
+++ proyecto/backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import os
+
 from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True,port=os.getenv('PORT'))
--- proyecto/backend/boot.bat
+++ proyecto/backend/boot.bat
@@ -1,1 +1,2 @@
 Scripts\activate
+python .\app.py
