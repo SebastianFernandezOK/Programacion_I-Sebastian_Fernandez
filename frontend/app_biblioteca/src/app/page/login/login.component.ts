3440cbd10cd72b5109843e758f7d02ca149b83ed
PochaOK <sebastianfernandez2812@gmail.com>
2024-09-24 00:17:19 -0300

    angular

--- frontend/app_biblioteca/src/app/page/login/login.component.ts
+++ frontend/app_biblioteca/src/app/page/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
