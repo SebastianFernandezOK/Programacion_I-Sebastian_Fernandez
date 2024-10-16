cc3cfa0d0dd081ea0efbb540b477202d3c00a9d6
PochaOK <sebastianfernandez2812@gmail.com>
2024-10-16 13:17:57 -0300

    logica usuarios

--- frontend/app_biblioteca/src/app/components/users/see-user/see-user.component.ts
+++ frontend/app_biblioteca/src/app/components/users/see-user/see-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosService } from '../../../services/usuarios.service';
 declare var window: any; // Esto es para usar Bootstrap Modal con JavaScript
 
 interface User {
@@ -17,99 +19,100 @@ interface User {
   styleUrls: ['./see-user.component.css']
 })
 export class SeeUserComponent {
-  users: User[] = [
-    { 
-      usuarioID: 1, 
-      nombre: 'John', 
-      apellido: 'Doe', 
-      email: 'johndoe@example.com', 
-      telefono: 1234567890, 
-      rol: 'User', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 2, 
-      nombre: 'Jane', 
-      apellido: 'Smith', 
-      email: 'janesmith@example.com', 
-      telefono: 9876543210, 
-      rol: 'Librarian', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 3, 
-      nombre: 'Admin', 
-      apellido: 'User', 
-      email: 'adminuser@example.com', 
-      telefono: 5551234567, 
-      rol: 'Admin', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 4, 
-      nombre: 'Michael', 
-      apellido: 'Johnson', 
-      email: 'michaeljohnson@example.com', 
-      telefono: 1231231234, 
-      rol: 'User', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 5, 
-      nombre: 'Emily', 
-      apellido: 'Davis', 
-      email: 'emilydavis@example.com', 
-      telefono: 3213214321, 
-      rol: 'Librarian', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 6, 
-      nombre: 'William', 
-      apellido: 'Brown', 
-      email: 'williambrown@example.com', 
-      telefono: 4564564567, 
-      rol: 'User', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 7, 
-      nombre: 'Olivia', 
-      apellido: 'Martinez', 
-      email: 'oliviamartinez@example.com', 
-      telefono: 7897897890, 
-      rol: 'Admin', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 8, 
-      nombre: 'James', 
-      apellido: 'Garcia', 
-      email: 'jamesgarcia@example.com', 
-      telefono: 6546546543, 
-      rol: 'User', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 9, 
-      nombre: 'Sophia', 
-      apellido: 'Wilson', 
-      email: 'sophiawilson@example.com', 
-      telefono: 9879879870, 
-      rol: 'Librarian', 
-      photo: 'descarga (1).png' 
-    },
-    { 
-      usuarioID: 10, 
-      nombre: 'Mason', 
-      apellido: 'Lopez', 
-      email: 'masonlopez@example.com', 
-      telefono: 3216549870, 
-      rol: 'Admin', 
-      photo: 'descarga (1).png' 
-    }
-  ];
+  
+  //   { 
+  //     usuarioID: 1, 
+  //     nombre: 'John', 
+  //     apellido: 'Doe', 
+  //     email: 'johndoe@example.com', 
+  //     telefono: 1234567890, 
+  //     rol: 'User', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 2, 
+  //     nombre: 'Jane', 
+  //     apellido: 'Smith', 
+  //     email: 'janesmith@example.com', 
+  //     telefono: 9876543210, 
+  //     rol: 'Librarian', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 3, 
+  //     nombre: 'Admin', 
+  //     apellido: 'User', 
+  //     email: 'adminuser@example.com', 
+  //     telefono: 5551234567, 
+  //     rol: 'Admin', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 4, 
+  //     nombre: 'Michael', 
+  //     apellido: 'Johnson', 
+  //     email: 'michaeljohnson@example.com', 
+  //     telefono: 1231231234, 
+  //     rol: 'User', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 5, 
+  //     nombre: 'Emily', 
+  //     apellido: 'Davis', 
+  //     email: 'emilydavis@example.com', 
+  //     telefono: 3213214321, 
+  //     rol: 'Librarian', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 6, 
+  //     nombre: 'William', 
+  //     apellido: 'Brown', 
+  //     email: 'williambrown@example.com', 
+  //     telefono: 4564564567, 
+  //     rol: 'User', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 7, 
+  //     nombre: 'Olivia', 
+  //     apellido: 'Martinez', 
+  //     email: 'oliviamartinez@example.com', 
+  //     telefono: 7897897890, 
+  //     rol: 'Admin', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 8, 
+  //     nombre: 'James', 
+  //     apellido: 'Garcia', 
+  //     email: 'jamesgarcia@example.com', 
+  //     telefono: 6546546543, 
+  //     rol: 'User', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 9, 
+  //     nombre: 'Sophia', 
+  //     apellido: 'Wilson', 
+  //     email: 'sophiawilson@example.com', 
+  //     telefono: 9879879870, 
+  //     rol: 'Librarian', 
+  //     photo: 'descarga (1).png' 
+  //   },
+  //   { 
+  //     usuarioID: 10, 
+  //     nombre: 'Mason', 
+  //     apellido: 'Lopez', 
+  //     email: 'masonlopez@example.com', 
+  //     telefono: 3216549870, 
+  //     rol: 'Admin', 
+  //     photo: 'descarga (1).png' 
+  //   }
+  // 
 
+  users: User[] = []
   selectedUser: User = {
     usuarioID: 0,
     nombre: '',
@@ -122,12 +125,28 @@ export class SeeUserComponent {
   
   editUserModal: any;
 
+  constructor(
+    private router: Router,
+    private usuariosService: UsuariosService
+  ){}
+    
   ngOnInit() {
-    // Inicializamos el modal al cargar el componente
-    this.editUserModal = new window.bootstrap.Modal(
-      document.getElementById('editUserModal')
-    );
+    this.usuariosService.getUsers().subscribe((rta:any) => {
+      console.log('usuarios api: ',rta);
+      this.users = rta.animales || [];
+      this.editUserModal = new window.bootstrap.Modal(
+        document.getElementById('editUserModal')
+      );
+      //this.filteredUsers = [...this.arrayUsuarios]
+    })
   }
+    
+  // ngOnInit() {
+  //   // Inicializamos el modal al cargar el componente
+  //   this.editUserModal = new window.bootstrap.Modal(
+  //     document.getElementById('editUserModal')
+  //   );
+  // }
 
   // Función para eliminar un usuario
   deleteUser(usuarioID: number): void {
